f6316f64d60f5793c6c76c58809c98eb66317d1c
Beto Toro <betotoro0902@gmail.com>
2022-11-06 12:43:12 -0500

    [feat] fix landscape behaviour when the app runs in a large device

---
+++ SnowSeeker/views/Welcome.swift
@@ -0,0 +1,26 @@
+//
+//  Welcome.swift
+//  SnowSeeker
+//
+//  Created by Beto Toro on 6/11/22.
+//
+
+import SwiftUI
+
+struct Welcome: View {
+  var body: some View {
+    VStack {
+      Text("Welcome to SnowSeeker!")
+        .font(.largeTitle)
+      
+      Text("Please select a resort from the left-hand menu; swipe from the left edge to show it.")
+        .foregroundColor(.secondary)
+    }
+  }
+}
+
+struct Welcome_Previews: PreviewProvider {
+  static var previews: some View {
+    Welcome()
+  }
+}
