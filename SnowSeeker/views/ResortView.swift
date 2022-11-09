cc1f160b192d84160feaad7fc9357d212c518fd6
Beto Toro <betotoro0902@gmail.com>
2022-11-09 10:59:17 -0500

    [feat] finish last project

--- SnowSeeker/views/ResortView.swift
+++ SnowSeeker/views/ResortView.swift
@@ -13,6 +13,11 @@ struct ResortView: View {
   @Environment(\.horizontalSizeClass) var sizeClass
   @Environment(\.dynamicTypeSize) var typeSize
   
+  @EnvironmentObject var favorites: Favorites
+  
+  @State private var selectedFacility: Facility?
+  @State private var showingFacility = false
+  
   var body: some View {
     ScrollView {
       VStack(alignment: .leading, spacing: 0) {
@@ -40,19 +45,46 @@ struct ResortView: View {
           Text("Facilities")
             .font(.headline)
           
-          Text(resort.facilities, format: .list(type: .and))
-            .padding(.vertical)
+          HStack {
+            ForEach(resort.facilityTypes) { facility in
+              Button {
+                selectedFacility = facility
+                showingFacility = true
+              } label: {
+                facility.icon
+                  .font(.title)
+              }
+            }
+          }
+          .padding(.vertical)
         }
         .padding(.horizontal)
       }
+      
+      Button(favorites.contains(resort) ? "Remove from Favorites" : "Add to Favorites") {
+        if favorites.contains(resort) {
+          favorites.remove(resort)
+        } else {
+          favorites.add(resort)
+        }
+      }
+      .buttonStyle(.borderedProminent)
+      .padding()
     }
     .navigationTitle("\(resort.name), \(resort.country)")
     .navigationBarTitleDisplayMode(.inline)
+    .alert(selectedFacility?.name ?? "More information", isPresented: $showingFacility, presenting: selectedFacility) { _ in  
+    } message: { facility in
+      Text(facility.description)
+    }
   }
 }
 
 struct ResortView_Previews: PreviewProvider {
   static var previews: some View {
-    ResortView(resort: Resort.example)
+    NavigationView {
+      ResortView(resort: Resort.example)
+    }
+    .environmentObject(Favorites())
   }
 }
