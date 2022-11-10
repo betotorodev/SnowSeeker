ebbb0920cc7c806513d44cf6a080d190fdf41367
Beto Toro <betotoro0902@gmail.com>
2022-11-10 11:45:47 -0500

    [feat] first challenge

--- SnowSeeker/views/ResortView.swift
+++ SnowSeeker/views/ResortView.swift
@@ -21,9 +21,18 @@ struct ResortView: View {
   var body: some View {
     ScrollView {
       VStack(alignment: .leading, spacing: 0) {
-        Image(decorative: resort.id)
-          .resizable()
-          .scaledToFit()
+        ZStack(alignment: .bottomTrailing) {
+          Image(decorative: resort.id)
+            .resizable()
+            .scaledToFit()
+          
+          Text("Photo by: \(resort.imageCredit)")
+            .font(.caption)
+            .padding(5)
+            .foregroundColor(.white)
+            .background(.primary)
+          
+        }
         
         HStack {
           if sizeClass == .compact && typeSize > .large {
